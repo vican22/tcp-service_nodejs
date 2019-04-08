4bd5e7a569eff7bbb766746586c813d78df945ba
Vedran <vedranknezevic1@yahoo.com>
2019-04-08 11:39:08 +0200

    Initial commit

---
+++ gateway-stream.js
@@ -0,0 +1,29 @@
+const net = require("net");
+const server = net.createServer();
+const JSONDuplexStream = require("json-duplex-stream");
+
+const Gateway = require("./gateway");
+
+server.on("connection", handleConnection);
+server.listen(8000, function() {
+  console.log("server listening on %j", server.address());
+});
+
+function handleConnection(conn) {
+  var s = JSONDuplexStream();
+  var gateway = Gateway();
+  conn
+    .pipe(s.in)
+    .pipe(gateway)
+    .pipe(s.out)
+    .pipe(conn);
+  s.in.on("error", onProtocolError);
+  s.out.on("error", onProtocolError);
+  conn.on("error", onConnError);
+  function onProtocolError(err) {
+    conn.end("protocol error:" + err.message);
+  }
+}
+function onConnError(err) {
+  console.error("connection error:", err.stack);
+}
